d2d314e012efc4982a31a647a3ea387b285576e4
张强 <350037310@qq.com>
2020-04-21 23:55:47 +0800

    feat: API root response

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-    getHello(): string {
-        return '123!';
+    private baseUrl = `http://127.0.0.1:3000`;
+    public getAllEndpoints() {
+        return {
+            '[EndPoint] For all endpoints': `GET ${this.baseUrl}`,
+            '[User API] Save a user'               : `POST ${this.baseUrl}/user/save body: {id: string, name: string, log: Log, password: string}`,
+            '[User API] Get one user'              : `GET  ${this.baseUrl}/user/:id `,
+            '[User API] Get user by limit & offset': `GET  ${this.baseUrl}/user?offset={offset}&limit={limit}`,
+            '[Log  API] Save a log '               : `POST ${this.baseUrl}/log body: {id: }`,
+            '[Log  API] Get one log'               : `GET  ${this.baseUrl}/log/:id`
+        };
     }
 }
