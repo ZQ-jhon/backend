5004be5198ab33e580cc3976576a5d351adf03ac
ZQ-jhon <350037310@qq.com>
2020-04-27 01:08:35 +0800

    fix: refix host to hostname

--- front-end/scripts/request.js
+++ front-end/scripts/request.js
@@ -1,6 +1,6 @@
 import { ajax } from './ajax.js';
 import { randomString } from './random-string.js';
-const baseUrl = `${location.host}:3000`;
+const baseUrl = `${location.hostname}:3000`;
 // 登录
 // <div>
 // <h4>登录测试</h4>
