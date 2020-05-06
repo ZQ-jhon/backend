6d31f9b1c8f4dc930dc6e7770b51a66ed83f29f7
ZQ-jhon <350037310@qq.com>
2020-05-06 22:34:07 +0800

    chore: rename swagger API paramater

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .addTag('ZQ-jhon')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
     app.enableCors();
     await app.listen(3000);
 }
