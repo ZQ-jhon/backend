f0be5f3db7f960c75174bcd4040c8096724bdc22
ZQ-jhon <350037310@qq.com>
2020-05-11 01:26:09 +0800

    fix: remove husky rules in commit hook

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,18 +10,15 @@ export class AppService {
             },
             auth: {
                 '[Auth API] Login                           ': 'POST ' + this.baseUrl + '/auth/login body: [UserDto]',
-                '[Auth API] Redeem token                    ':
-                    'POST ' + this.baseUrl + '/auth/token header [Bearer authorization]',
+                '[Auth API] Redeem token                    ': 'POST ' + this.baseUrl + '/auth/token header [Bearer authorization]',
                 '[Auth API] Create user                     ': 'POST ' + this.baseUrl + '/auth/user body [UserDto]',
             },
             user: {
                 '[User API] GET one user                    ': 'GET  ' + this.baseUrl + '/user?${userIdOrUsername}',
-                '[User API] GET users                       ':
-                    'GET  ' + this.baseUrl + '/user?offset={offset}&limit={limit}',
+                '[User API] GET users                       ': 'GET  ' + this.baseUrl + '/user?offset={offset}&limit={limit}',
             },
             comment: {
-                '[comment  API] POST one comment            ':
-                    'POST ' + this.baseUrl + '/comment body: {userId: string, content: string }',
+                '[comment  API] POST one comment            ': 'POST ' + this.baseUrl + '/comment body: {userId: string, content: string }',
                 '[comment  API] GET one comment             ': 'GET  ' + this.baseUrl + '/comment/:id',
             },
         };
