4f27ac29061955f1b3207af47ce641d4f0e56877
ZQ-jhon <350037310@qq.com>
2020-05-17 22:41:09 +0800

    chore: 🔧 split database to a module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
@@ -8,23 +7,15 @@ import { UserModule } from './modules/user/user.module';
 import { LogModule } from './modules/log/log.module';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
     imports: [
+        LogModule,
+        AuthModule,
         UserModule,
         CommentModule,
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'root',
-            password: '1234',
-            database: 'test',
-            entities: ['dist/**/**.entity{.ts,.js}'], // sb webpack
-            synchronize: true,
-        }),
-        AuthModule,
-        LogModule,
+        DatabaseModule,
     ],
     controllers: [AppController],
     providers: [
---
+++ src/modules/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: '1234',
+            database: 'test',
+            entities: ['dist/**/**.entity{.ts,.js}'], // sb webpack
+            synchronize: true,
+        }),
+    ]
+})
+export class DatabaseModule {}
