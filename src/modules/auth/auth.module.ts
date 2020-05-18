873d09448031007b7a075fd521e9a0d75821361b
ZQ-jhon <350037310@qq.com>
2020-05-19 00:01:22 +0800

    chore: adding jwt secret

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JWT_SECRET } from '../../constants/jwt-secret';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User]), JwtModule.register({ secret: 'hello' })],
+    imports: [TypeOrmModule.forFeature([User]), JwtModule.register({ secret: JWT_SECRET })],
     controllers: [AuthController],
     providers: [AuthService],
 })
