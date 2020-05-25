36106defa8b5d53685f8473d157bbca651e95ae2
ZQ-jhon <350037310@qq.com>
2020-05-25 22:06:15 +0800

    chore: make code clear

--- src/modules/redis-cache/redis-cache.service.ts
+++ src/modules/redis-cache/redis-cache.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { KeyType, Redis, ValueType } from 'ioredis';
 import { RedisService } from 'nestjs-redis';
-import { REDIS_INJECT_TOKEN } from '../../constants/redis-inject-token';
-import { Redis, KeyType, ValueType } from 'ioredis';
 import { from } from 'rxjs';
+import { REDIS_INJECT_TOKEN } from '../../constants/redis-inject-token';
 @Injectable()
 export class RedisCacheService {
     private client: Redis;
@@ -16,20 +16,10 @@ export class RedisCacheService {
         this.client = await this.redisService.getClient(REDIS_INJECT_TOKEN);
     }
     public set(key: KeyType, value: ValueType, expire?: string | any[]) {
-        if (expire) {
-            return from(this.client.set(key, value, expire));
-        } else {
-            return from(this.client.set(key, value));
-        }
+        return from(this.client.set(key, value, expire));
     }
     public get(key: KeyType) {
         return from(this.client.get(key));
-    }
-    public publish(channel: string, message: string) {
-        return from(this.client.publish(channel, message));
-    }
-    public subscribe(channel: string) {
-        return from(this.client.subscribe(channel));
     }
     public empty() {
         return from(this.client.flushall());
